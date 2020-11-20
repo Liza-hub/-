4ae61189d99ed7271da3e905e6c3f7d688ec9458
Liza <54031870+Liza-hub@users.noreply.github.com>
2020-11-21 00:31:11 +0300

    Create source.h

---
+++ source.h
@@ -0,0 +1,21 @@
+#ifndef PROJECT_INCLUDE_SOURCE_H_
+#define PROJECT_INCLUDE_SOURCE_H_
+
+typedef struct masterRecord1 {
+  int Number;
+  char Name[20];
+  char Surname[20];
+  char addres[30];
+  char TelNumber[15];
+  double indebtedness;
+  double credit_limit;
+  double cash_payments;
+} Data;
+
+/* typedef struct masterRecord2 {
+  int Number;
+  double cash_payments;
+} DataTransfer; */
+
+
+#endif  // PROJECT_INCLUDE_SOURCE_H_
