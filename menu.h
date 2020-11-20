abbcd065f0b3dfdc81c351aae878844dcea5d7f3
Liza <54031870+Liza-hub@users.noreply.github.com>
2020-11-21 00:30:36 +0300

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,42 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "source.h"
-#include "utils.h"
-
-void write_to_file_client(FILE *f, Data *client) {
-  fprintf(f, "%-12d%-11s%-11s%-16s%20s%12.2f%12.2f%12.2f\n", client->Number,
-                                                                 client->Name,
-                                                                 client->Surname,
-                                                                 client->addres,
-                                                                 client->TelNumber,
-                                                                 client->indebtedness,
-                                                                 client->credit_limit ,
-                                                                 client->cash_payments);
-}
+#ifndef PROJECT_INCLUDE_MENU_H_
+#define PROJECT_INCLUDE_MENU_H_
 
 
-int read_from_file_client(FILE *f, Data *client_data) {
-  int res = 0;
-  res = fscanf(f, "%10d%10s%10s%10s%10s%10lf%10lf%10lf", &client_data->Number,
-                                                     client_data->Name,
-                                                     client_data->Surname,
-                                                     client_data->addres,
-                                                     client_data->TelNumber,
-                                                     &client_data->indebtedness,
-                                                     &client_data->credit_limit,
-                                                     &client_data->cash_payments);
-  return res;
-}
+void input_client();
+void input_transfer();
+void menu_choice();
 
 
-void write_to_file_transfer(FILE *f, Data *transfer) {
-  fprintf(f, "%-3d%-5.2f\n", transfer->Number, transfer->cash_payments);
-}
-
-
-int read_from_file_transfer(FILE *f, Data *transfer) {
-  int res = 0;
-  res = fscanf(f, "%d %lf", &transfer->Number, &transfer->cash_payments);
-  return res;
-}
+#endif  // PROJECT_INCLUDE_MENU_H_
