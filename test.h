efed0104777d1a2b2f4e857336c17dd5c97acbd9
Liza <54031870+Liza-hub@users.noreply.github.com>
2020-11-21 00:31:46 +0300

    Create test.h

---
+++ test.h
@@ -0,0 +1,17 @@
+#ifndef PROJECT_INCLUDE_TEST_H_
+#define PROJECT_INCLUDE_TEST_H_
+
+
+#include "structfile.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define filename "transaction.dat";
+
+int some(Data * data1, Data *data2);
+
+void test_write_to_file();
+
+
+#endif  // PROJECT_INCLUDE_TEST_H_
